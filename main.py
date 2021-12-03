56de8a8bdb612ee0b7c51c55b3b12c2bc5b901dd
mrcruz117 <mrcruz1989@gmail.com>
2021-12-03 15:56:26 -0500

    snake is controlable.

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+from snake import Snake
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
-screen.bgcolor('black')
-screen.title('Snake Game')
-starting_pos = 0
+screen.bgcolor("black")
+screen.title("My Snake Game")
+screen.tracer(0)
 
-segment_1 = Turtle('square')
-segment_1.color('white')
+snake = Snake()
 
-segment_2 = Turtle('square')
-segment_2.color('white')
-segment_2.goto(-20, 0)
 
-segment_3 = Turtle('square')
-segment_3.color('white')
-segment_3.goto(-40, 0)
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.06)
+    snake.move()
+
 
 screen.exitonclick()
