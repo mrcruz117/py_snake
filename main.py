67b54d5f6dcc43d1348b43cbcc2c192f4e76e033
mrcruz117 <mrcruz1989@gmail.com>
2021-12-10 14:11:42 -0500

    tail collision handled

--- main.py
+++ main.py
@@ -35,11 +35,18 @@ while game_is_on:
     # detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend()
         scoreboard.increase_score()
-
+    # wall collision
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         scoreboard.game_over()
         game_is_on = False
-
+    # tail collision
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.game_over()
+            game_is_on = False
 
 screen.exitonclick()
