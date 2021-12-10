ff1f154eb17a74052f560a4cd84a4ca510802d23
mrcruz117 <mrcruz1989@gmail.com>
2021-12-10 11:31:39 -0500

    fixed  pycharm recognition issue. and code formatting.

--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
@@ -8,12 +9,11 @@ class Scoreboard(Turtle):
         self.score = 0
         self.color("white")
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", align="center",font=("Ariel", 20 , "normal"))
+        self.write(f"Score: {self.score}", align="center", font=("Ariel", 20, "normal"))
 
     def update_scoreboard(self):
-        print(updated)
+        print("updated")
 
-
-    def increase_score( self):
-        self.score+=1
-        self.write(f"Score: {self.score}", align="center",font=("Ariel", 20 , "normal"))
+    def increase_score(self):
+        self.score += 1
+        self.write(f"Score: {self.score}", align="center", font=("Ariel", 20, "normal"))
