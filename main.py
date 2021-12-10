234e4d8befcb77caccceb0270578850684e2d07d
mrcruz117 <mrcruz1989@gmail.com>
2021-12-10 13:46:40 -0500

    game over on wall collision

--- main.py
+++ main.py
@@ -29,13 +29,17 @@ screen.onkey(snake.right, "Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.06)
+    time.sleep(0.1)
     snake.move()
 
     # detect collision with food.
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.increase_score()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        scoreboard.game_over()
+        game_is_on = False
 
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -8,13 +8,17 @@ class Scoreboard(Turtle):
         self.penup()
         self.score = 0
         self.color("white")
-        self.goto(0, 270)
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align="center", font=("Ariel", 20, "normal"))
+        self.goto(0, 270)
+        self.write(f"Score: {self.score}", align="center", font=("Courier", 20, "normal"))
 
     def increase_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER...", align="center", font=("Courier", 20, "normal"))
