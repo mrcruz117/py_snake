289c4535c5fe1272d97212d7bf1498d590ed2548
mrcruz117 <mrcruz1989@gmail.com>
2021-12-10 11:06:10 -0500

    updating score

--- scoreboard.py
+++ scoreboard.py
@@ -10,6 +10,10 @@ class Scoreboard(Turtle):
         self.goto(0, 270)
         self.write(f"Score: {self.score}", align="center",font=("Ariel", 20 , "normal"))
 
+    def update_scoreboard(self):
+        print(updated)
+
+
     def increase_score( self):
         self.score+=1
         self.write(f"Score: {self.score}", align="center",font=("Ariel", 20 , "normal"))
